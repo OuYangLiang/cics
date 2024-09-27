c49274846ca0ead5c856e0c1b80fa93c99d77100
OuYangLiang <ouyanggod@gmail.com>
2024-09-27 14:20:37 +0800

    Initial commit

---
+++ cics-aggregation/src/main/java/com/oyl/cics/Application.java
@@ -0,0 +1,14 @@
+package com.oyl.cics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * author: OuYangLiang
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
