6f1b771a69f207ee86c127d8fbab76ad4e500f29
OuYangLiang <ouyanggod@gmail.com>
2024-09-27 22:55:29 +0800

    增加mysql数据库数据源

---
+++ cics-aggregation/src/main/java/com/oyl/cics/DataSourceConfig.java
@@ -0,0 +1,32 @@
+package com.oyl.cics;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+
+@Configuration
+public class DataSourceConfig {
+
+    private static final int DEFAULT_POOL_SIZE = 10;
+
+    @Bean
+    public DataSource dataSource() throws PropertyVetoException {
+        ComboPooledDataSource rlt = new ComboPooledDataSource();
+//        rlt.setDriverClass("com.mysql.jdbc.Driver");
+        rlt.setDriverClass("com.mysql.cj.jdbc.Driver");
+        rlt.setUser("root");
+        rlt.setPassword("pwd12345");
+        rlt.setJdbcUrl("jdbc:mysql://localhost:3306/cics");
+        rlt.setInitialPoolSize(DEFAULT_POOL_SIZE);
+        rlt.setMaxPoolSize(DEFAULT_POOL_SIZE);
+        rlt.setMinPoolSize(DEFAULT_POOL_SIZE);
+        rlt.setTestConnectionOnCheckin(true);
+        rlt.setPreferredTestQuery("select 1");
+
+        return rlt;
+    }
+
+}
--- cics-web/src/main/java/com/oyl/cics/web/login/LoginController.java
+++ cics-web/src/main/java/com/oyl/cics/web/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.oyl.cics.web.login;
 
+import com.oyl.cics.model.user.User;
+import com.oyl.cics.model.user.UserRepos;
 import com.oyl.cics.web.common.result.RestResult;
 import com.oyl.cics.web.common.util.JwtUtil;
 import org.springframework.validation.BindingResult;
@@ -7,18 +9,25 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import javax.validation.Valid;
 
 @RestController
 public class LoginController {
 
+    @Resource
+    private UserRepos userRepos;
+
     @RequestMapping(path="/login")
     public RestResult<String> login(@RequestBody @Valid LoginRequest loginRequest, BindingResult bindingResult) {
-        if ("OYL".equals(loginRequest.getUsername()) && "pwd123".equals(loginRequest.getPassword())) {
-            return RestResult.ok(JwtUtil.instance.createToken(loginRequest.getUsername()));
+
+        User user = userRepos.queryByUsername(loginRequest.getUsername());
+
+        if (null == user || !user.getPassword().equals(loginRequest.getPassword())) {
+            return RestResult.fail("1", "用户名 或 密码 不正确");
         }
 
-        return RestResult.fail("1", "用户名 或 密码 不正确");
+        return RestResult.ok(JwtUtil.instance.createToken(loginRequest.getUsername()));
     }
 
 }
