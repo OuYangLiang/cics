1022eeed9ffaf794145b4868358c492fab4b3714
OuYangLiang <ouyanggod@gmail.com>
2024-10-08 22:24:10 +0800

    增加两个工具类

---
+++ cics-model/src/main/java/com/oyl/cics/model/common/utils/MD5Encryptor.java
@@ -0,0 +1,37 @@
+package com.oyl.cics.model.common.utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public enum MD5Encryptor {
+    inst;
+
+    public String getMD5(String input) {
+        try {
+            // 获取MD5算法的MessageDigest实例
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            // 更新摘要以包含指定的字节数组（输入字符串的字节表示）
+            md.update(input.getBytes());
+
+            // 计算哈希值，结果为字节数组
+            byte[] digest = md.digest();
+
+            // 将字节数组转换为十六进制字符串
+            StringBuilder sb = new StringBuilder();
+            for (byte b : digest) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /*public static void main(String[] args) {
+        String input = "Hello, World!";
+        String md5Hash = MD5Encryptor.inst.getMD5(input);
+        System.out.println("MD5 hash of '" + input + "' is: " + md5Hash);
+    }*/
+}
---
+++ cics-model/src/main/java/com/oyl/cics/model/common/utils/RandomGenerator.java
@@ -0,0 +1,27 @@
+package com.oyl.cics.model.common.utils;
+
+import java.util.Random;
+
+public enum RandomGenerator {
+    inst;
+
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final Random RANDOM = new Random();
+
+    public String strs(int length) {
+        StringBuilder stringBuilder = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int index = RANDOM.nextInt(CHARACTERS.length());
+            stringBuilder.append(CHARACTERS.charAt(index));
+        }
+        return stringBuilder.toString();
+    }
+
+    /*public static void main(String[] args) {
+        for (int i = 0; i < 200; i++) {
+            String randomString = RandomGenerator.inst.strs(6);
+            System.out.println("生成的6位随机字符串是: " + randomString);
+        }
+    }*/
+
+}
