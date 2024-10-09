1e93bee7c735fba7cab245d8d483376afe3c6ea8
OuYangLiang <ouyanggod@gmail.com>
2024-10-09 15:44:06 +0800

    增加两个实体类

---
+++ cics-model/src/main/java/com/oyl/cics/model/guidaoheng/Guidaoheng.java
@@ -0,0 +1,311 @@
+package com.oyl.cics.model.guidaoheng;
+
+import java.util.List;
+
+public class Guidaoheng {
+
+    /**
+     * 明细磅单号
+     */
+    private String zmxdocNo;
+
+    /**
+     * 所属二级公司
+     */
+    private String ssejdw;
+
+    /**
+     * 所属二级公司代码
+     */
+    private String ssejdwid;
+
+    /**
+     * 所属三级公司
+     */
+    private String sssjdw;
+
+    /**
+     * 所属三级公司代码
+     */
+    private String sssjdwid;
+
+    /**
+     * 装车站点
+     */
+    private String zczhandian;
+
+    /**
+     * 装车站点编码
+     */
+    private String zczhandianbm;
+
+    /**
+     * 车次
+     */
+    private String zccheci;
+
+    /**
+     * 车型
+     */
+    private String chexing;
+
+    /**
+     * 煤源矿点名称
+     */
+    private String mykuangdianmc;
+
+    /**
+     * 煤源矿点编码
+     */
+    private String mykuangdianbm;
+
+    /**
+     * 煤炭来源
+     */
+    private String meitanlaiyuan;
+
+    /**
+     * 煤炭来源编码
+     */
+    private String meitanlaiyuanbm;
+
+    /**
+     * 收货单位名称（终到用户）
+     */
+    private String zname1;
+
+    /**
+     * 收货单位编号（终到用户）
+     */
+    private String zkunnr;
+
+    /**
+     * 销售组织描述
+     */
+    private String vtext;
+
+    /**
+     * 销售组织代码
+     */
+    private String vkorg;
+
+    /**
+     * 首车号
+     */
+    private String shouchehao;
+
+    /**
+     * 尾车号
+     */
+    private String weichehao;
+
+    /**
+     * 车数
+     */
+    private Long cheshu;
+
+    /**
+     * 计量单位
+     */
+    private String jiliangdanwei;
+
+    /**
+     * 总票重
+     */
+    private Double zongpiaozhong;
+
+    /**
+     * 总毛重
+     */
+    private Double zongmaozhong;
+
+    /**
+     * 总皮重
+     */
+    private Double zongpizhong;
+
+    /**
+     * 总净重
+     */
+    private Double zongjingzhong;
+
+    /**
+     * 总扣吨量
+     */
+    private Double koudunzongliang;
+
+    /**
+     * 结算煤量
+     */
+    private Double jiesuanmeiliang;
+
+    /**
+     * 总盈亏
+     */
+    private Double zongyingkui;
+
+    /**
+     * 总盈吨
+     */
+    private Double zongyingdun;
+
+    /**
+     * 总亏吨
+     */
+    private Double zongkuidun;
+
+    /**
+     * 总运损
+     */
+    private Double zongyunsun;
+
+    /**
+     * 过衡时间
+     */
+    private String duiweisj;
+
+    /**
+     * 过衡日期
+     */
+    private String duiweirq;
+
+    /**
+     * 业务日期
+     */
+    private String yewurq;
+
+    /**
+     * 煤种
+     */
+    private String zcmeizhong;
+
+    /**
+     * 煤种编码
+     */
+    private String zcmeizhongbm;
+
+    /**
+     * 外购煤供应商
+     */
+    private String wgmgys;
+
+    /**
+     * 数据源系统
+     */
+    private String sjyxt;
+
+    /**
+     * 车源
+     */
+    private String cheyuan;
+
+    /**
+     * 运单号
+     */
+    private String yundanhaoyundanhao;
+
+    /**
+     * 计量类型
+     */
+    private String jltypetype;
+
+    /**
+     * 计量衡器用途
+     */
+    private String jlhqyt;
+
+    /**
+     * 计量衡器名称
+     */
+    private String jlhqmc;
+
+    /**
+     * 计量衡器ID
+     */
+    private String jlhqmcid;
+
+    /**
+     * 到站
+     */
+    private String daozhan;
+
+    /**
+     * 发站
+     */
+    private String fazhan;
+
+    /**
+     * 终到站
+     */
+    private String zhongdao;
+
+    /**
+     * 数据推送单位
+     */
+    private String sjtsdanwei;
+
+    /**
+     * 数据推送日期
+     */
+    private String xtscjlI;
+    /**
+     * 数据推送时间
+     */
+    private String xtscjlT;
+
+    /**
+     * 备用字段1
+     */
+    private String byzd1;
+
+    /**
+     * 备用字段2
+     */
+    private String byzd2;
+
+    /**
+     * 备用字段3
+     */
+    private String byzd3;
+
+    /**
+     * 备用字段4
+     */
+    private String byzd4;
+
+    /**
+     * 备用字段5
+     */
+    private String byzd5;
+
+    /**
+     * 备用字段6
+     */
+    private String byzd6;
+
+    /**
+     * 备用字段7
+     */
+    private String byzd7;
+
+    /**
+     * 备用字段8
+     */
+    private String byzd8;
+
+    /**
+     * 备用字段9
+     */
+    private String byzd9;
+
+    /**
+     * 备用字段10
+     */
+    private String byzd10;
+
+    /**
+     * 详情数据
+     */
+    private List<GuidaohengDetail> dtData;
+
+}
---
+++ cics-model/src/main/java/com/oyl/cics/model/guidaoheng/GuidaohengDetail.java
@@ -0,0 +1,114 @@
+package com.oyl.cics.model.guidaoheng;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GuidaohengDetail {
+    /**
+     * 明细磅单号
+     */
+    private String zmxdocNo;
+
+    /**
+     * 每车详情-序号
+     */
+    private String dtSortno;
+
+    /**
+     * 每车详情-车号
+     */
+    private String dtChehao;
+
+    /**
+     * 每车详情-车型
+     */
+    private String dtChexing;
+
+    /**
+     * 每车详情-毛重
+     */
+    private Double dtMaozhong;
+
+    /**
+     * 每车详情-皮重
+     */
+    private Double dtPizhong;
+
+    /**
+     * 每车详情-净重
+     */
+    private Double dtJingzhong;
+
+    /**
+     * 每车详情-标重吨数
+     */
+    private Double dtBzdunshu;
+
+    /**
+     * 每车详情-票重
+     */
+    private Double dtPiaozhong;
+
+    /**
+     * 每车详情-盈吨
+     */
+    private Double dtYingdun;
+
+    /**
+     * 每车详情-亏吨
+     */
+    private Double dtKuidun;
+
+    /**
+     * 每车详情-运损
+     */
+    private Double dtYunsun;
+
+    /**
+     * 每车详情-扣吨
+     */
+    private Double dtKoudun;
+
+    /**
+     * 每车详情-检重时间
+     */
+    private String dtCztime;
+
+    /**
+     * 每车详情-检轻时间
+     */
+    private String dtCqtime;
+
+    /**
+     * 每车详情-过衡速度
+     */
+    private Double dtGhsudu;
+
+    /**
+     * 每车详情-预留字段1
+     */
+    private String dtReserve1;
+
+    /**
+     * 每车详情-预留字段2
+     */
+    private String dtReserve2;
+
+    /**
+     * 每车详情-预留字段3
+     */
+    private String dtReserve3;
+
+    /**
+     * 每车详情-预留字段4
+     */
+    private String dtReserve4;
+
+    /**
+     * 每车详情-预留字段5
+     */
+    private String dtReserve5;
+
+}
