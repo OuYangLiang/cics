5c9593d74548cb57a10d8f49c2be90255670f5fd
OuYangLiang <ouyanggod@gmail.com>
2024-10-22 23:46:54 +0800

    双数据源

--- cics-impl/src/test/java/com/oyl/cics/impl/DataSourceConfig.java
+++ cics-impl/src/test/java/com/oyl/cics/impl/DataSourceConfig.java
@@ -3,6 +3,7 @@ package com.oyl.cics.impl;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 import java.beans.PropertyVetoException;
@@ -11,7 +12,16 @@ import java.beans.PropertyVetoException;
 public class DataSourceConfig {
 
     @Bean
+    @Primary
     public DataSource dataSource() throws PropertyVetoException {
+        ComboPooledDataSource rlt = new ComboPooledDataSource();
+        rlt.setDriverClass("org.h2.Driver");
+        rlt.setJdbcUrl("jdbc:h2:mem:test;MODE=MySQL");
+        return rlt;
+    }
+
+    @Bean
+    public DataSource dataSourceForOracle() throws PropertyVetoException {
         ComboPooledDataSource rlt = new ComboPooledDataSource();
         rlt.setDriverClass("org.h2.Driver");
         rlt.setJdbcUrl("jdbc:h2:mem:test;MODE=MySQL");
